17eeb2a32c793ec18f5ec3b43ae5a175897fd40a
JackBowden98 <78490280+JackBowden98@users.noreply.github.com>
2021-02-17 18:12:29 +0000

    Fixed hopefully

---
+++ Prototype/Assets/Scripts/CollectableManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectableManager : MonoBehaviour
+{
+    public static CollectableManager instance;
+    [SerializeField] private int souls = 0;
+    [SerializeField] private Text soulText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void IncrementScore(int soulValue)
+    {
+        souls += soulValue;
+        soulText.text = "Souls: " + souls.ToString();
+    }
+}
---
+++ Prototype/Assets/Scripts/SmallSoul.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallSoul : MonoBehaviour
+{
+    public int soulValue = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            CollectableManager.instance.IncrementScore(soulValue);
+        }
+    }
+}
